6303f665f7ee4bcf2eab834fb2b1d90c8109a7b0
Alan Monroy Bernedo <alan.monroy@ucsp.edu.pe>
2024-06-25 15:18:15 -0500

    Please work

--- src/lib/aws.js
+++ src/lib/aws.js
@@ -1,10 +1,10 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } from '$env/static/private';
+import { AWS_ACCESSKEYID, AWS_SECRETACCESSKEY } from '$env/static/private';
 
 export const s3Client = new S3Client({
 	region: "us-east-2",
 	credentials: {
-	  accessKeyId: AWS_ACCESS_KEY_ID,
-	  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+	  accessKeyId: AWS_ACCESSKEYID,
+	  secretAccessKey: AWS_SECRETACCESSKEY,
 	},
 });
--- src/routes/search/+page.server.js
+++ src/routes/search/+page.server.js
@@ -2,7 +2,7 @@ import { error } from "@sveltejs/kit"
 
 export async function load({ url }) {
 
-	const query = url.searchParams.get('query')
+	const query = url.searchParams.get('query').toLowerCase()
 	const res = await fetch(`https://whro5umnmb.execute-api.us-east-2.amazonaws.com/dev/Buscador/query?query=${query}`)
 	
 	if (!res.ok) {
